465b5c5c8f28a0027521239f5bb3a7d17602170c
nahuy <1312240@student.hcmus.edu.vn>
2018-07-07 15:01:38 +0700

    Devide 3 lop

--- app/src/main/java/com/example/lap10715/study_git/ui/MainActivity.java
+++ app/src/main/java/com/example/lap10715/study_git/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.lap10715.study_git;
+package com.example.lap10715.study_git.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.lap10715.study_git.R;
 
 public class MainActivity extends AppCompatActivity {
 
